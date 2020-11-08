a4a992fd73a9720eb57e948701474756dea6e7d3
abmmhasan <abmmhasan346@gmail.com>
2020-11-08 13:45:41 +0600

    Init

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # OTP
-Simple but Secure TOTP &amp; HOTP solution!
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+
+
+Simple but Secure TOTP (RFC6238) & HOTP (RFC4226) solution!
+
+
+## Prerequisites
+
+Language: PHP 7.1/+
+
+## Installation
+
+```
+composer require abmmhasan/otp
+```
+
+## Why this library?
+
+Most of the OTP library found there (written in PHP) are insecure. Wanna know why?
+1. Uses Online URL to generate QR Images (it exposes your secret key, online)
+2. Uses basic Base32 functions which is not Time safe (verifies same OTP upto 90 second instead of 30 second)
+
+Well, this library mitigates both problems using [Constant-Time Encoding](https://github.com/paragonie/constant_time_encoding) & [QR Code generator](https://github.com/Bacon/BaconQrCode).
+All the data generated are simply on your very own server.
+
+## Usage
+
+### HOTP
+
+```php
+/**
+* Generate Secret
+* It will generate secure random secret string
+*/
+$secret = (new \AbmmHasan\OTP\OTP())->createSecret();
+
+/**
+* Get QR Code Image for secret $secret
+*/
+(new \AbmmHasan\OTP\OTP())->setSecret($secret)->getQRSnap('hotp','TestName','TestTitle');
+
+/**
+* Get current OTP for a given counter
+*/
+$counter = 346;
+$otp = (new \AbmmHasan\OTP\OTP())->setSecret($secret)->getHOTP($counter);
+
+/**
+* Verify
+*/
+(new \AbmmHasan\OTP\OTP())->setSecret($secret)->verify($otp,$counter);
+// or
+(new \AbmmHasan\OTP\OTP())->setSecret($secret)->verify($otp,$counter,'hotp');
+```
+
+### TOTP
+
+```php
+/**
+* Generate Secret
+* It will generate secure random secret string
+*/
+$secret = (new \AbmmHasan\OTP\OTP())->createSecret();
+
+/**
+* Get QR Code Image for secret $secret
+*/
+(new \AbmmHasan\OTP\OTP())->setSecret($secret)->getQRSnap('totp','TestName','TestTitle');
+
+/**
+* Get current OTP
+*/
+$otp = (new \AbmmHasan\OTP\OTP())->setSecret($secret)->getTOTP();
+// or get OTP for another specified epoch time
+$otp = (new \AbmmHasan\OTP\OTP())->setSecret($secret)->getTOTP(1604820275);
+/**
+* Verify current OTP
+*/
+(new \AbmmHasan\OTP\OTP())->setSecret($secret)->verify($otp);
+// or verify for a specified time
+(new \AbmmHasan\OTP\OTP())->setSecret($secret)->verify($otp,1604820275,'totp');
+```
+
+## Support
+
+Having trouble? Create an issue!
---
+++ src/OTP/OTP.php
@@ -0,0 +1,142 @@
+<?php
+
+
+namespace AbmmHasan\OTP;
+
+
+use BaconQrCode\Renderer\Image\SvgImageBackEnd;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
+use ParagonIE\ConstantTime\Base32;
+
+class OTP
+{
+    private $secret;
+
+    /**
+     * Generate Secret
+     *
+     * @return string 16 digit secret
+     * @throws \Exception
+     */
+    public function createSecret()
+    {
+        $string = strtoupper(bin2hex(random_bytes(5)));
+        $string = Base32::encodeUpper($string);
+        return trim(mb_strtoupper($string), '=');
+    }
+
+    /**
+     * Generate QR Code for secret
+     *
+     * @param string $otp_type Otp type: totp or hotp
+     * @param string $name
+     * @param null|string $title
+     * @return string
+     */
+    public function getQRSnap(string $otp_type, string $name, string $title = null)
+    {
+        if (!in_array($otp_type, ['totp', 'hotp'])) {
+            throw new \InvalidArgumentException('Only totp or hotp type allowed');
+        }
+        $url = "otpauth://{$otp_type}/{$name}?secret={$this->secret}";
+        if (isset($title)) {
+            $url .= '&issuer=' . $title;
+        }
+        $writer = new Writer(
+            new ImageRenderer(
+                new RendererStyle(200),
+                new SvgImageBackEnd()
+            )
+        );
+        return $writer->writeString($url);
+    }
+
+    /**
+     * Set secret for further process
+     *
+     * @param string $secret
+     * @return $this
+     */
+    public function setSecret(string $secret)
+    {
+        $this->secret = $secret;
+        return $this;
+    }
+
+    /**
+     * Get TOTP code based on input or for current time
+     *
+     * @param int|null $input
+     * @return int
+     */
+    public function getTOTP(int $input = null)
+    {
+        return self::getOTP($input);
+    }
+
+    /**
+     * Get HOTP code based on input counter
+     *
+     * @param int $counter
+     * @return int
+     */
+    public function getHOTP(int $counter)
+    {
+        return self::getOTP($counter, 6, 1);
+    }
+
+    /**
+     * Verify OTP
+     *
+     * @param int $otp
+     * @param int|null $input
+     * @param string|null $type
+     * @return bool
+     */
+    public function verify(int $otp, int $input = null, string $type = null)
+    {
+        switch (strtolower($type)) {
+            case 'totp':
+                return $otp == self::getTOTP($input ?? time());
+            case 'hotp':
+                return $otp == self::getHOTP($input);
+            default:
+                if (is_null($input)) {
+                    return $otp == self::getTOTP();
+                }
+                return $otp == self::getHOTP($input);
+        }
+    }
+
+    /**
+     * Get OTP for current time or specified input
+     *
+     * @param int|null $input
+     * @param int $digits
+     * @param int $interval
+     * @return int
+     */
+    private function getOTP(int $input = null, int $digits = 6, int $interval = 30)
+    {
+        $input = $input ?? time();
+        $timeCode = ($input * 1000) / ($interval * 1000);
+        $result = $hmac = [];
+        while ($timeCode != 0) {
+            $result[] = chr($timeCode & 0xFF);
+            $timeCode >>= 8;
+        }
+        $intToByteString = str_pad(join(array_reverse($result)), 8, "\000", STR_PAD_LEFT);
+        $hash = hash_hmac('sha1', $intToByteString, Base32::decodeUpper($this->secret));
+        foreach (str_split($hash, 2) as $hex) {
+            $hmac[] = hexdec($hex);
+        }
+        $offset = $hmac[19] & 0xf;
+        $code = ($hmac[$offset + 0] & 0x7F) << 24 |
+            ($hmac[$offset + 1] & 0xFF) << 16 |
+            ($hmac[$offset + 2] & 0xFF) << 8 |
+            ($hmac[$offset + 3] & 0xFF);
+        return $code % pow(10, $digits);
+    }
+}
