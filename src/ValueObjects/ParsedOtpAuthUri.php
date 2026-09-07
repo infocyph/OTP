1cb63a037d4db1149b78e336765b8390311d6e9f
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-07 09:44:43 +0600

    security: redact parsed OTP secrets from debug output

--- src/ValueObjects/ParsedOtpAuthUri.php
+++ src/ValueObjects/ParsedOtpAuthUri.php
@@ -8,6 +8,7 @@ final readonly class ParsedOtpAuthUri
 {
     public function __construct(
         public string $type,
+        #[\SensitiveParameter]
         public string $secret,
         public string $label,
         public ?string $issuer,
@@ -19,4 +20,34 @@ final readonly class ParsedOtpAuthUri
         /** @var array<string, string> */
         public array $additionalParameters = [],
     ) {}
+
+    /**
+     * @return array{
+     *     type:string,
+     *     secret:string,
+     *     label:string,
+     *     issuer:?string,
+     *     algorithm:string,
+     *     digits:int,
+     *     period:?int,
+     *     counter:?int,
+     *     ocraSuite:?string,
+     *     additionalParameters:array<string,string>
+     * }
+     */
+    public function __debugInfo(): array
+    {
+        return [
+            'type' => $this->type,
+            'secret' => '[redacted]',
+            'label' => $this->label,
+            'issuer' => $this->issuer,
+            'algorithm' => $this->algorithm,
+            'digits' => $this->digits,
+            'period' => $this->period,
+            'counter' => $this->counter,
+            'ocraSuite' => $this->ocraSuite,
+            'additionalParameters' => $this->additionalParameters,
+        ];
+    }
 }
